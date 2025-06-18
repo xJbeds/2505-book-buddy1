7d4453d028ac8117b368ab4b29b7ce05d6440d15
Jake Bedford <robertjakebedford@gmail.com>
2025-06-18 17:59:50 -0600

    fixing more css

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router";
 import { useAuth } from "../auth/AuthContext";
 
+
+
 const Navbar = () => {
   const { token, logout } = useAuth();
   return (
     <header>
+      <NavLink to="/books">
       <b>Book Buddy</b>
+      </NavLink>
       <nav>
         {token ? (
           <a onClick={() => logout()}>Log out</a>
